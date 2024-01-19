b70992d04b8e050f7dcb3999ae44a4129eeeb408
Thisaru Supun <thisarusupun1314@gmail.com>
2024-01-19 21:00:25 +0530

    Create SignInKey

--- src/main/java/com/thisarusupun/springsecurity/config/JwtService.java
+++ src/main/java/com/thisarusupun/springsecurity/config/JwtService.java
@@ -2,10 +2,16 @@ package com.thisarusupun.springsecurity.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "MMbLsxSaXdivbyHKdr9j+2yr8nhDghfmm8N+pRp/Jc0T4ukg+BkcsppWT1AN0Vwc";
 
     public String extractUsername(String jwt) {
         return null;
@@ -18,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
